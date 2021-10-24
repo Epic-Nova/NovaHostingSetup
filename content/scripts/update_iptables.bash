76c1f6f540fcbf30afd22305a7ad5c85076dacc4
root <root@Programmer-Hood.localdomain>
2021-10-24 19:26:12 +0200

    Mods

--- content/scripts/update_iptables.bash
+++ content/scripts/update_iptables.bash
@@ -21,6 +21,6 @@ echo "$i" >> $LOGFILE
 done
 
 iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 22 -j DROP
-iptables -I INPUT -s localhost -j ACCEPT
+iptables -A INPUT -s localhost -j ACCEPT
 iptables-save
 echo "Updated SSH Access Table"
